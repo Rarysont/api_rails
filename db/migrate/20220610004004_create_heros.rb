4d73181061af6b24f8c0005441aba0264ce58b86
Raryson Taylor <rarysont@gmail.com>
2022-06-09 21:55:06 -0300

    add fields null

--- db/migrate/20220610004004_create_heros.rb
+++ db/migrate/20220610004004_create_heros.rb
@@ -1,7 +1,7 @@
 class CreateHeros < ActiveRecord::Migration[7.0]
   def change
     create_table :heros do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
