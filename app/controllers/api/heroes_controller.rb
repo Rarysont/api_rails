6d1fa526e424f0ee92ac713731f3a6e40fed60c2
Raryson Taylor <rarysont@gmail.com>
2022-06-11 20:16:42 -0300

    fix token

--- app/controllers/api/heroes_controller.rb
+++ app/controllers/api/heroes_controller.rb
@@ -1,4 +1,8 @@
 class Api::HeroesController < ApplicationController
+  include Authenticable
+
+  before_action :authenticate_with_token, except: %i[index show]
+
   before_action :set_hero, only: %i[show update destroy]
 
   # GET /heroes
